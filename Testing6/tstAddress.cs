c39572493cf9fdacc12e381778c3c4cddab6228b
Matthew Dean <mjdean@dmu.ac.uk>
2022-01-05 14:21:29 +0000

    Added testing for address class

---
+++ Testing6/tstAddress.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing6
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAddress AnAddress = new clsAddress();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
